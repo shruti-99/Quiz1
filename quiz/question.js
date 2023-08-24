b12e9839b88123681850e823de22106b5c58b741
shruti-99 <124458606+shruti-99@users.noreply.github.com>
2023-08-24 20:25:52 +0530

    Add files via upload

---
+++ quiz/question.js
@@ -0,0 +1,48 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Regression testing is primarily related to",
+    answer: "Functional testing",
+    options: [
+      "Maintenance testing",
+      "Data flow testing",
+      "Functional testing",
+      "Development testing"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which one of the following is an application of Stack Data Structure?",
+    answer: "All of the above",
+    options: [
+      "Managing function calls",
+      "The stock span problem",
+      "Arithmetic expression evaluation",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Of the following sort algorithms, which has execution time that is least dependent on initial ordering of the input ?",
+    answer: "Merge sort",
+    options: [
+      "Selection sort",
+      "Insertion sort",
+      "Quick sort",
+      "Merge sort"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following traversal outputs the data in sorted order in a BST?",
+    answer: "Inorder",
+    options: [
+      "Preorder",
+      "Inorder",
+      "Postorder",
+      "Level Order "
+    ]
+  },
+    
+];
+
